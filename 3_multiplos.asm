5f60d8d474c0c28d4f28551ca8e63c9c71a8fc20
SebastianHurtado12 <69992546+SebastianHurtado12@users.noreply.github.com>
2020-10-05 00:47:09 -0300

    Update 3_multiplos.asm

---
+++ 3_multiplos.asm
@@ -0,0 +1,41 @@
+.data
+    newLine: .asciiz "\n "
+
+.text
+.globl main
+main:
+
+  li    $t0, 1               # inicializamos la variabel para el conteo
+  la    $t3, 101             # inicializamos la variabel para el conteo
+
+loop:
+
+
+  beq  $t0, $t3, exit        # if(t0==t3) goto exit
+  rem  $t1, $t0, 5           # t1 = t0 % 5  verificamos si el numero es divisible por 5
+  beq  $t1, $zero, imprimir  # if(t1==0) goto imprimir el numero en cuestion SI es multiplo de 5
+  rem  $t1, $t0, 5           # t1 = t0 % 3  verificamos si el numero es divisible por 3
+  beq  $t1, $zero, imprimir  # if(t3==0) goto imprimir el numero en cuestion SI es multiplo de 3
+
+  addi $t0, $t0, 1      # count ++
+
+  j loop                # repite el loop
+
+imprimir:                    # imprimios los multiplos en la consola
+
+  li  $v0, 1
+  move  $a0, $t0
+  syscall               # imprimimos el multiplo
+
+  li $v0, 4
+  la $a0, newLine
+  syscall               # realizamos un salto de linea
+
+  addi $t0, $t0, 1      # count ++
+
+  j loop                # repite el loop
+
+exit:                  # termina el programa
+
+  li   $v0, 10        # syscall exit code
+  syscall             # termina el programa
